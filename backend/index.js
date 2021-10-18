f0fa2c9802aa473e5a99e923e47d1c2fe4fb80f8
Namyalg <namyalg@gmail.com>
2021-10-18 12:09:31 +0530

    Final updates to backend(b)

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,22 @@
+/*
+    This file is used to set up the backend server, consists of the imports and routes
+*/
+
+//imports and dependencies
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-
 const cors = require('cors')
 require('dotenv/config')
+
+//middlewares
 const app = express()
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-//middlewares
 app.use(cors())
 
-//imports and requests
-const uri = process.env.DB
 //make the database connection
-
+const uri = process.env.DB
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -24,17 +26,17 @@ mongoose.connect(uri, {
     console.log("error is" + err)
 })
 
-
 app.get('/test', (req, res) => {
     res.json({message : "This is a test route"})
 })
 
+//the login and sign up functions are routed here
 const userRouter = require("./routes/UserRouter")
 app.use('/user', userRouter)
 
+//adding, deleting, updating contacts are routed here
 const userContactRouter = require("./routes/UserContactRouter")
 app.use('/contact', userContactRouter)
-
 
 var port = process.env.PORT || 9001
 app.listen(port, function(){
